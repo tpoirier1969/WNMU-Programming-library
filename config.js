cc5f7e682e288982a738b0154e3a891e5cad74d1
Tod <tpoirier@nmu.edu>
2026-03-23 10:29:51 -0400

    Disable authentication requirement in config
    Changed REQUIRE_AUTH from true to false in config.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://tdepltlnughyfrjqufdg.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_oz-1MPs6ix3grIJ7dCbOZg_jYpw6_Q1",
-  REQUIRE_AUTH: true,
+  REQUIRE_AUTH: false,
   AUTO_ARCHIVE_DAYS: 90,
   APP_TITLE: "WNMU-TV Programming",
   APP_VERSION: "v1.3.2"
