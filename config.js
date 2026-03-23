74d6b70c47b91e5997f5c34a6717af85a2ee1686
Tod <tpoirier@nmu.edu>
2026-03-23 10:20:01 -0400

    Change REQUIRE_AUTH value from yes to true

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://tdepltlnughyfrjqufdg.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_oz-1MPs6ix3grIJ7dCbOZg_jYpw6_Q1",
-  REQUIRE_AUTH: yes,
+  REQUIRE_AUTH: true,
   AUTO_ARCHIVE_DAYS: 90,
   APP_TITLE: "WNMU-TV Programming",
   APP_VERSION: "v1.3.2"
